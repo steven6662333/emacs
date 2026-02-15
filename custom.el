dc42b288bd3e336645fc140ccc4d03e1c8e0f820
Steven Shen <1289158387@qq.com>
2026-02-15 12:16:09 +0800

    ✨ feat(custom.el): sync `custom.el`

--- custom.el
+++ custom.el
@@ -4,13 +4,31 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("e4a702e262c3e3501dfe25091621fe12cd63c7845221687e36a79e17cf3a67e0"
+     "921f165deb8030167d44eaa82e85fcef0254b212439b550a9b6c924f281b5695"
+     "720838034f1dd3b3da66f6bd4d053ee67c93a747b219d1c546c41c4e425daf93"
+     "9e5e0ff3a81344c9b1e6bfc9b3dcf9b96d5ec6a60d8de6d4c762ee9e2121dfb2"
+     "4d5d11bfef87416d85673947e3ca3d3d5d985ad57b02a7bb2e32beaf785a100e"
+     "7771c8496c10162220af0ca7b7e61459cb42d18c35ce272a63461c0fc1336015"
+     "0325a6b5eea7e5febae709dab35ec8648908af12cf2d2b569bedc8da0a3a81c1"
+     "aec7b55f2a13307a55517fdf08438863d694550565dee23181d2ebd973ebd6b8"
+     "a5c590aeb7dc5c2b8d36601a4c94a1145e46bd2291571af02807dd7a8552630c"
+     "c8078cccd38e52c3f94822b0b2bbe83886dea993536acfde2db019f46a193503"
+     default))
+ '(doom-1337-padded-modeline nil)
  '(package-selected-packages
-   '(centered-cursor-mode corfu counsel easy-theme-preview
-			  evil-collection evil-surround general
-			  good-scroll key-chord magit rotate windsize)))
+   '(centered-cursor-mode colorful-mode corfu doom-themes evil-collection
+			  evil-surround flycheck-eglot general gptel
+			  key-chord kirigami magit marginalia
+			  nerd-icons-corfu org-roam pinentry
+			  rainbow-delimiters rainbow-mode rotate
+			  rust-mode sis vertico vterm vterm-toggle
+			  windsize)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(font-lock-comment-face ((t (:foreground "#6D6D6D"))))
+ '(mode-line ((t (:background "#68217A")))))
